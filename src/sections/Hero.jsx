21799a36059a9c710a9af13b2fe36900c2923ad9
yinnyeinhtwe <kangjiinee@gmail.com>
2026-05-22 13:27:15 +0630

    feat: Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -61,7 +61,7 @@ export default function Hero() {
           </div>
           <div className="flex items-center gap-5">
             <a href="#work" className="btn-primary px-6 py-3 mt-4 rounded-full bg-black text-white transition-all duration-300 hover:scale-110 active:scale-95">View Work</a>
-            <a href="/public/YinNyeinHtwe.pdf" download className="btn-outline px-6 py-3 mt-4 rounded-full bg-black text-white transition-all duration-300 hover:scale-110 active:scale-95">Download CV</a>
+            <a href="/YinNyeinHtwe.pdf" download className="btn-outline px-6 py-3 mt-4 rounded-full bg-black text-white transition-all duration-300 hover:scale-110 active:scale-95">Download CV</a>
           </div>
         </div>
 
